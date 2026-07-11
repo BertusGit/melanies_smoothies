de5fcd099d834ca4f0c1c9e631ccbe632775c13a
BertusGit <34538680+BertusGit@users.noreply.github.com>
2026-07-11 13:46:49 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,4 +59,4 @@ import requests
 #smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
 # Correct:
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.jsaon())
