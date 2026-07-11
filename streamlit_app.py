d0fc7dc396d9842f30af6f3f7f5f05d65c29e8fc
BertusGit <34538680+BertusGit@users.noreply.github.com>
2026-07-11 13:56:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,9 @@ import os
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
+import requests  
+
+
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothe :cup_with_straw:")
 st.write(
@@ -41,6 +44,8 @@ if ingredients_List:
 
     for fruit_chosen in ingredients_List:
         ingredients_string += fruit_chosen + '    '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     st.write(ingredients_string)
 
@@ -55,9 +60,4 @@ if ingredients_List:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
 
-import requests  
-#smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-# Correct:
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
